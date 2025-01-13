803362a685e2f832ae9aa65e3731f532723b5271
epugliese27 <epugliese27@riverdale.k12.or.us>
2025-01-13 10:43:14 -0800

    h

--- selectchar.php
+++ selectchar.php
@@ -2,8 +2,16 @@
 require_once('connect.php');
 require('layout.html');
 session_start();
-$user=$_SESSION['user'];
-echo('<div id="logoutBanner"><span id="showUser" style="float:left;">User: '.$user.' <a href="mainpage.php">Go Back? </a></span> <a href="logout.php">Log Out </a> </div><br>');
+$username=$_SESSION['user'];
+$sql=$conn->prepare("SELECT email FROM user WHERE pkuser = :username");
+$sql->bindValue(":username", $username);
+$result=$sql->execute();
+$rows = $sql->fetchAll(PDO::FETCH_ASSOC);
+echo('<div id="logoutBanner"><span id="showUser" style="float:left;">User: '.$rows[0]["email"].' <a href="mainpage.php">Go Back? </a> </span> <a href="logout.php">Log Out </a> </div><br>');
+$sql=$conn->prepare("SELECT * FROM tblChars WHERE userID = :user");
+$sql->bindValue(":user", $username);
+$result=$sql->execute();
+
 $charid=$_GET['id'];
 echo("selected character id: ".$charid."<br>");
 $sql=$conn->prepare("SELECT * FROM tblChars WHERE id = :charid");
