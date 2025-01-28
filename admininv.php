0568d2a6c2fae0240271ee6f26bf6b680c42d12f
epugliese27 <epugliese27@riverdale.k12.or.us>
2025-01-28 10:48:58 -0800

    table

--- admininv.php
+++ admininv.php
@@ -18,13 +18,18 @@ FROM user
 $result=$sql->execute();
 $rows = $sql->fetchAll(PDO::FETCH_ASSOC);
 echo("<div class='adminbox'>");
-echo("users <br>");
-
+echo("users
+<table>
+<tr> <th>user id</th><th>username</th> </tr>");
 foreach($rows as $row){
-    echo("<strong>".$row['pkuser']."</strong> ");
-    echo($row['email'] ."<br>");
-};
+    echo("<tr><th><strong>".$row['pkuser']."</strong></th>");
+
+    echo("<th>".$row['email']."</th>");
+
+}
+echo("</table>");
 echo("</div>");
+
 $sql=$conn->prepare(" SELECT user.email,tblChars.id,tblChars.firstname,tblChars.lastname
 FROM tblChars 
  LEFT JOIN user
@@ -33,13 +38,16 @@ ON tblChars.userID = user.pkuser;
 $result=$sql->execute();
 $rows = $sql->fetchAll(PDO::FETCH_ASSOC);
 echo("<div class='adminbox'>");
-echo("chars <br>");
+echo("chars
+<table>
+<tr> <th>email</th><th>character id</th><th>fname</th><th>lname</th> </tr>");
 foreach($rows as $row){
-    echo("<strong>".$row['email']."</strong> ");
-    echo("<strong>".$row['id']."</strong> ");
-    echo($row['firstname']." ");
-    echo($row['lastname'] ."<br>");
+    echo("<tr><th><strong>".$row['email']."</strong></th>");
+    echo("<th><strong>".$row['id']."</strong> </th>");
+    echo("<th>".$row['firstname']."</th>");
+    echo("<th>".$row['lastname'] ."</th></tr>");
 }
+echo("</table>");
 echo("</div>");
 $sql=$conn->prepare("SELECT tblChars.id,tblChars.firstname,tblinv.itemname,tblinv.itemtype,tblinv.amt
 FROM tblChars 
@@ -48,13 +56,18 @@ ON tblChars.id = tblinv.fkchar;
 ");
 $result=$sql->execute();
 $rows = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+
 echo("<div class='adminbox'>");
-echo("items <br>");
+echo("items
+<table>
+<tr> <th>char id</th><th>char fname</th><th>item name</th><th>item type</th><th>item amt</th> </tr>");
 foreach($rows as $row){
-    echo("<strong>".$row['id']."</strong> ");
-    echo("<strong>".$row['firstname']."</strong> ");
-    echo($row['itemname']." ");
-    echo($row['itemtype']." ");
-    echo("[".$row['amt'] ."] <br>");
+    echo("<tr><th><strong>".$row['id']."</strong></th>");
+    echo("<th><strong>".$row['firstname']."</strong> </th>");
+    echo("<th>".$row['itemname']."</th>");
+    echo("<th>".$row['itemtype']."</th>");
+    echo("<th>[".$row['amt'] ."]</th></tr>");
 }
+echo("</table>");
 echo("</div>");
